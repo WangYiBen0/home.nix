700abbe9a33e9e5c75f9756437ea725f82549c0c
W1ngD1nGa5ter <wangyiben1@outlook.com>
2026-04-06 18:47:23 +0800

    Add xdg base directory

--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,6 @@
 _: {
   imports = [
+    ./directory.nix
     ./file.nix
     ./general.nix
     ./identity.nix
---
+++ modules/directory.nix
@@ -0,0 +1,30 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+let
+  links = {
+    "桌面" = "desktop";
+    "文档" = "documents";
+    "下载" = "download";
+    "音乐" = "music";
+    "图片" = "pictures";
+    "视频" = "videos";
+    "模板" = "templates";
+    "公共" = "publicShare";
+  };
+in
+{
+  xdg.userDirs = {
+    enable = true;
+    # createDirectories = true; # 可能导致现有程序崩溃，造成大批量重启淹没终端输出
+    setSessionVariables = true;
+  };
+
+  home.file = lib.mapAttrs (chineseName: xdgKey: {
+    source = config.lib.file.mkOutOfStoreSymlink config.xdg.userDirs.${xdgKey};
+  }) links;
+}

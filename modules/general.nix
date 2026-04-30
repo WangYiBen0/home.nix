975902545469e17d3367ef1601a83ac5cd4e95e4
W1ngD1nGa5ter <wangyiben1@outlook.com>
2026-04-30 21:47:48 +0800

    feat(general): add whitesur theme

--- modules/general.nix
+++ modules/general.nix
@@ -1,8 +1,40 @@
-{ inputs, ... }:
+{
+  inputs,
+  pkgs,
+  ...
+}:
 {
   imports = [
     inputs.catppuccin.homeModules.catppuccin
   ];
+
+  home.packages = with pkgs; [
+    libsForQt5.qtstyleplugin-kvantum
+    kdePackages.qtstyleplugin-kvantum
+    whitesur-kde
+    whitesur-icon-theme
+    whitesur-cursors
+    whitesur-gtk-theme
+  ];
+
+  qt = {
+    enable = true;
+    platformTheme.name = "qtct";
+    style.name = "kvantum";
+  };
+
+  xdg.configFile."Kvantum/kvantum.kvconfig".text = ''
+    [General]
+    theme=WhiteSurDark
+  '';
+
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "WhiteSur-dark";
+      package = pkgs.whitesur-icon-theme;
+    };
+  };
 
   catppuccin = {
     enable = true;
@@ -14,6 +46,12 @@
     };
     nvim = {
       flavor = "macchiato";
+    };
+    gtk.icon = {
+      enable = false;
+    };
+    kvantum = {
+      apply = false;
     };
   };
 }

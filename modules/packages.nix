c181331f4ea51a9f9819d023ceb3cf2d1e7fc700
W1ngD1nGa5ter <wangyiben1@outlook.com>
2026-07-02 20:55:28 +0800

    feat(packages): add nix-your-shell

--- modules/packages.nix
+++ modules/packages.nix
@@ -48,6 +48,12 @@
     fzf.enable = true;
     jq.enable = true;
     nh.enable = true;
+
+    nix-your-shell = {
+      enable = true;
+      nix-output-monitor.enable = true;
+    };
+
     ripgrep.enable = true;
     ripgrep-all.enable = true;
     uv.enable = true;
